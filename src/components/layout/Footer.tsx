dae3e8f0163dfe957bf88b8315128dfb2ded13eb
Sarthak Agarwal <sarrthak@amazon.com>
2023-05-16 22:40:20 +0100

    code modularisation for footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,80 +1,70 @@
-import { component$ } from "@builder.io/qwik";
+import {component$} from "@builder.io/qwik";
+
+interface LinkItemProps {
+  href: string;
+  title?: string;
+  spanClassName?: string;
+  ariaLabel?: string;
+}
 
 export default component$(() => {
   return (
-    <footer class="main-footer u-cross-center">
-      <div class="main-footer-start">
-        <ul class="inline-links is-no-padding-first-and-last u-x-small">
-          <li class="inline-links-item">
-            <a
-              href="https://appwrite.io/policy/terms"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span class="text">Terms</span>
-            </a>
-          </li>{" "}
-          <li class="inline-links-item">
-            <a
-              href="https://appwrite.io/policy/privacy"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span class="text">Privacy</span>
-            </a>
-          </li>
-          <li class="inline-links-item">
-            <a
-              href="https://appwrite.io/policy/cookies"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span class="text">Cookies</span>
-            </a>
-          </li>
-        </ul>
-      </div>{" "}
-      <div class="main-footer-end">
-        <ul class="inline-links is-no-padding-first-and-last u-x-small">
-          <li class="inline-links-item">
-            <div class="u-flex u-cross-center u-gap-8">
-              <span class="icon-cloud "></span>{" "}
-              <a
-                class="text"
-                href="https://cloud.appwrite.io/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Powered by Appwrite Cloud
-              </a>
-            </div>
-          </li>
-          <li class="inline-links-item u-flex u-gap-8">
-            <a
-              href="https://github.com/appwrite/built-with"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span
-                class="icon-github"
-                aria-hidden="true"
-                aria-label="Appwrite on Github"
-              ></span>
-            </a>
-            <a
-              href="https://appwrite.io/discord"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span
-                class="icon-discord"
-                aria-hidden="true"
-                aria-label="Appwrite on Discord"
-              ></span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+      <footer class="main-footer u-cross-center">
+        <div class="main-footer-start">
+          <PolicyLinks/>
+        </div>
+        {" "}
+        <div class="main-footer-end">
+          <SocialLinks/>
+        </div>
+      </footer>
   );
 });
+
+function PolicyLinks() {
+  return (
+      <ul class="inline-links is-no-padding-first-and-last u-x-small">
+        <PolicyLink href="https://appwrite.io/policy/terms" title="Terms"/>
+        <PolicyLink href="https://appwrite.io/policy/privacy" title="Privacy"/>
+        <PolicyLink href="https://appwrite.io/policy/cookies" title="Cookies"/>
+      </ul>
+  );
+}
+
+function SocialLinks() {
+  return (
+      <ul class="inline-links is-no-padding-first-and-last u-x-small">
+        <li class="inline-links-item">
+          <div class="u-flex u-cross-center u-gap-8">
+            <span class="icon-cloud "></span>
+            <LinkItem href="https://cloud.appwrite.io/" title="Powered by Appwrite Cloud"/>
+          </div>
+        </li>
+        <li class="inline-links-item u-flex u-gap-8">
+          <LinkItem href="https://github.com/appwrite/built-with" spanClassName="icon-github"
+                    ariaLabel="Appwrite on Github"/>
+          <LinkItem href="https://appwrite.io/discord" spanClassName="icon-discord"
+                    ariaLabel="Appwrite on Discord"/>
+        </li>
+      </ul>
+  );
+}
+
+function PolicyLink({href, title}: LinkItemProps) {
+  return (
+      <li className="inline-links-item">
+        <LinkItem href={href} title={title}/>
+      </li>
+  );
+}
+
+function LinkItem({href, title, spanClassName, ariaLabel}: LinkItemProps) {
+  return (
+      <a href={href} target="_blank" rel="noreferrer">
+        {title && <span className="text">{title}</span>}
+        {spanClassName && ariaLabel && (
+            <span className={spanClassName} aria-hidden="true" aria-label={ariaLabel}></span>
+        )}
+      </a>
+  );
+}
