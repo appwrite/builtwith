08e9b443dbf596d0b7f16d6cd3f0db9464d0a37d
loks0n <22452787+loks0n@users.noreply.github.com>
2023-05-19 17:41:33 +0100

    feat: add viewport meta tag

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,10 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" type="image/png" href="/favicon.png" />
 
