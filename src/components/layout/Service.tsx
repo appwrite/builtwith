fe49536c4386aff2a89f56c672f6f426957ad787
loks0n <22452787+loks0n@users.noreply.github.com>
2023-05-19 16:39:43 +0100

    style: new services section

--- src/components/layout/Service.tsx
+++ src/components/layout/Service.tsx
@@ -1,29 +1,22 @@
-import { component$ } from "@builder.io/qwik";
+import { Slot, component$ } from "@builder.io/qwik";
 import { useNavigate } from "@builder.io/qwik-city";
 
-export default component$(
-  (props: { id: string; name: string; image: string }) => {
-    const nav = useNavigate();
+export default component$((props: { id: string; name: string }) => {
+  const nav = useNavigate();
 
-    return (
-      <button onClick$={async () => await nav("/search?service=" + props.id)}>
-        <div
-          class="card u-flex-vertical u-cross-center u-main-center"
-          style="padding: 0px;"
-        >
-          <div class="object-square">
-            <img
-              style="border-radius: var(--border-radius-medium); border-bottom-left-radius: 0px !important; border-bottom-right-radius: 0px !important;"
-              src={props.image}
-              alt="Service"
-            />
-          </div>
+  return (
+    <button
+      class="card u-flex-vertical u-cross-center u-main-space u-overflow-hidden"
+      style="padding: 0px; width: 100%; aspect-ratio: 1 / 1.2;"
+      onClick$={async () => await nav("/search?service=" + props.id)}
+    >
+      <div class="c-service-background u-stretch">
+        <Slot />
+      </div>
 
-          <div style="padding: var(--p-card-padding)">
-            <p class="u-text-center eyebrow-heading-3">{props.name}</p>
-          </div>
-        </div>
-      </button>
-    );
-  }
-);
+      <div style="padding: var(--p-card-padding)">
+        <p class="u-text-center eyebrow-heading-3">{props.name}</p>
+      </div>
+    </button>
+  );
+});
--- src/components/layout/ServiceList.tsx
+++ src/components/layout/ServiceList.tsx
@@ -1,46 +1,41 @@
 import { component$ } from "@builder.io/qwik";
 import Service from "./Service";
+import Databases from "../icons/Databases";
+import Auth from "../icons/Auth";
+import Storage from "../icons/Storage";
+import Functions from "../icons/Functions";
+import Realtime from "../icons/Realtime";
 
 export default component$(() => {
   return (
     <ul
       class="grid-box"
-      style="--grid-gap:1rem; --grid-item-size:12rem; --grid-item-size-small-screens:8rem;"
+      style="--grid-gap:1rem; --grid-item-size:10rem; --grid-item-size-small-screens:8rem;"
     >
       <li>
-        <Service
-          id="databases"
-          name="Databases"
-          image="/images/services-db.png"
-        />
+        <Service id="databases" name="Databases">
+          <Databases />
+        </Service>
       </li>
       <li>
-        <Service
-          id="authentication"
-          name="Authentication"
-          image="/images/services-auth.png"
-        />
+        <Service id="authentication" name="Authentication">
+          <Auth />
+        </Service>
       </li>
       <li>
-        <Service
-          id="storage"
-          name="Storage"
-          image="/images/services-storage.png"
-        />
+        <Service id="storage" name="Storage">
+          <Storage />
+        </Service>
       </li>
       <li>
-        <Service
-          id="functions"
-          name="Functions"
-          image="/images/services-functions.png"
-        />
+        <Service id="functions" name="Functions">
+          <Functions />
+        </Service>
       </li>
       <li>
-        <Service
-          id="realtime"
-          name="Realtime"
-          image="/images/services-realtime.png"
-        />
+        <Service id="realtime" name="Realtime">
+          <Realtime />
+        </Service>
       </li>
     </ul>
   );
