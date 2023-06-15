67348660604b92e770ca85ee95ff5047784020e6
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-15 11:37:24 +0100

    feat: app 404

--- src/routes/projects/[projectId]/index.tsx
+++ src/routes/projects/[projectId]/index.tsx
@@ -50,23 +50,21 @@ export default component$(() => {
 
   if (!project) {
     return (
-      <>
-        <ul class="u-flex u-gap-24 u-flex-vertical-mobile">
-          <div class="u-flex-vertical u-gap-24 u-flex-shrink-0 u-flex-basis-50-percent">
-            <Link href="/" style="padding: 0px;" class="button is-text">
-              <span class="icon-cheveron-left" aria-hidden="true"></span>
-              <span class="text">Back to Projects</span>
-            </Link>
+      <ul class="u-flex u-gap-24 u-flex-vertical-mobile">
+        <div class="u-flex-vertical u-gap-24 u-flex-shrink-0 u-flex-basis-50-percent">
+          <Link href="/" style="padding: 0px;" class="button is-text">
+            <span class="icon-cheveron-left" aria-hidden="true"></span>
+            <span class="text">Back to Projects</span>
+          </Link>
 
-            <div>
-              <h2 class="heading-level-2">Not Found</h2>
-              <p class="u-margin-block-start-16" style="font-size: 1rem;">
-                This project does not exist.
-              </p>
-            </div>
+          <div>
+            <h2 class="heading-level-2">Not Found</h2>
+            <p class="u-margin-block-start-16" style="font-size: 1rem;">
+              This project does not exist.
+            </p>
           </div>
-        </ul>
-      </>
+        </div>
+      </ul>
     );
   }
 
