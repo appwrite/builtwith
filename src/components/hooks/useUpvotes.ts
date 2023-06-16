6f206524d7743f6da25ca4fe72cb60970291aa8e
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-16 16:36:24 +0100

    chore: remove redundant checks

--- src/components/hooks/useUpvotes.ts
+++ src/components/hooks/useUpvotes.ts
@@ -9,28 +9,26 @@ export function useUpvotes(projectIds: Signal<string[]>) {
   const account = useContext(AccountContext);
 
   useVisibleTask$(async ({ track }) => {
-    track(() => [account?.value, projectIds.value]);
-    if (!account?.value) {
+    track(() => [account.value, projectIds.value]);
+    if (!account.value) {
       return;
     }
 
-    if (account.value) {
-      const newProjectIds = projectIds.value.filter(
-        (projectId) => upvotes[projectId] === undefined
-      );
+    const newProjectIds = projectIds.value.filter(
+      (projectId) => upvotes[projectId] === undefined
+    );
 
-      if (newProjectIds.length === 0) {
-        return;
-      }
-
-      const projectUpvotes = await AppwriteService.listUserUpvotes(
-        account.value.$id,
-        [Query.equal("projectId", newProjectIds)]
-      );
-
-      projectUpvotes.forEach((upvote) => {
-        upvotes[upvote.projectId] = true;
-      });
+    if (newProjectIds.length === 0) {
+      return;
     }
+
+    const projectUpvotes = await AppwriteService.listUserUpvotes(
+      account.value.$id,
+      [Query.equal("projectId", newProjectIds)]
+    );
+
+    projectUpvotes.forEach((upvote) => {
+      upvotes[upvote.projectId] = true;
+    });
   });
 }
